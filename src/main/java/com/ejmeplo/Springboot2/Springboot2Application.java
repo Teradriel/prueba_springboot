5e6cc9a10deea05df22f8fc5730fa95f64769957
Luca <luca8973@hotmail.com>
2022-03-20 14:26:11 +0100

    eliminadas annotations sacadas de stackoverflow

--- src/main/java/com/ejmeplo/Springboot2/Springboot2Application.java
+++ src/main/java/com/ejmeplo/Springboot2/Springboot2Application.java
@@ -2,14 +2,8 @@ package com.ejmeplo.Springboot2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan({"com.delivery.request"})
-@EntityScan("com.delivery.domain")
-@EnableJpaRepositories("com.delivery.repository")
 public class Springboot2Application {
 
 	public static void main(String[] args) {
